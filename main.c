3b35f4241bc51806a0384978a355d0da5baa403d
Marcus Schimizzi <marcjschimizzi@gmail.com>
2018-04-30 17:53:40 +0100

    updated

--- main.c
+++ main.c
@@ -18,25 +18,46 @@ how to use the page table and disk interfaces.
 
 struct disk *disk;
 const char *replace;
-int frame_table[100];
+int* frame_table;
+int nframes;
 
 void page_fault_handler( struct page_table *pt, int page )
 {
 	int block = disk_nblocks(disk);
-	int frame = 0;
+	int frame = -1;
 	int bits = 0;
+
+	printf("page: %d\n", page);
+	printf("frame table: ");
+	int j;
+	for (j=0; j < nframes; j++) {
+		printf("%d ", frame_table[j]);
+	}
+	printf("\n");
+	
 	int i;
-	for(i = 0;i < block;i++){
-		if(!(frame_table[i])){
+	for(i = 0;i < nframes;i++){
+		if(frame_table[i] == 0){
 			frame = i;
 			break;
+		}else{
+			frame_table[i]++;
 		}
 	}
-	if(!(frame)){
+	if(frame == -1){
 		if(!strcmp(replace, "rand")){
-			frame = rand() % block;
+			frame = rand() % nframes;
 		} else if(!strcmp(replace, "fifo")) {
-			
+			// find oldest frame in use
+			int i, high = frame_table[0], high_frame = 0; 
+			for (i = 1; i < nframes; i++) {
+				if (frame_table[i] > high) {
+					high = frame_table[i];
+					high_frame = i;
+				}
+			}
+			frame = high_frame;
+			printf("old frame: %d\n", frame);
 		} else if(!strcmp(replace, "custom")) {
 	
 		} else {
@@ -44,9 +65,12 @@ void page_fault_handler( struct page_table *pt, int page )
 			exit(1);
 		}
 	}
-	page_table_get_entry(pt, page, &frame, &bits);
+	printf("frame: %d\n", frame);
+	int newframe;
+	page_table_get_entry(pt, page, &newframe, &bits);
 	if(bits&PROT_READ && !(bits&PROT_WRITE)){
-		page_table_set_entry(pt, page, frame, PROT_READ|PROT_WRITE);
+		printf("here\n");
+		page_table_set_entry(pt, page, newframe, PROT_READ|PROT_WRITE);
 	} else if (bits&PROT_WRITE) {
 		int npage = rand() % page_table_get_npages(pt);
 		char * mem = page_table_get_physmem(pt);
@@ -54,8 +78,9 @@ void page_fault_handler( struct page_table *pt, int page )
 		disk_read(disk, page, &mem[3*block]);
 		page_table_set_entry(pt, page, frame, PROT_READ);
 		page_table_set_entry(pt, npage, frame, 0);
-		frame_table[frame] = 0;
+		frame_table[frame] = 1;
 	} else {
+		printf("newentry\n");
 		page_table_set_entry(pt, page, frame, PROT_READ);
 		char * mem = page_table_get_physmem(pt);
 		disk_read(disk, page, &mem[3*block]);
@@ -73,7 +98,7 @@ int main( int argc, char *argv[] )
 	}
 
 	int npages = atoi(argv[1]);
-	int nframes = atoi(argv[2]);
+	nframes = atoi(argv[2]);
 	replace = argv[3];
 	const char *program = argv[4];
 
@@ -89,6 +114,7 @@ int main( int argc, char *argv[] )
 		return 1;
 	}
 
+	frame_table = (int*) calloc(nframes, sizeof(int));
 	char *virtmem = page_table_get_virtmem(pt);
 
 	char *physmem = page_table_get_physmem(pt);
@@ -108,6 +134,7 @@ int main( int argc, char *argv[] )
 	}
 
 	page_table_delete(pt);
+	free(frame_table);
 	disk_close(disk);
 
 	return 0;
